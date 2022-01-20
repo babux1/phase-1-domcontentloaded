3c2b11fe301deee6352b77849ea9f37785129fb7
batmunkh namshir <namshir17@gmail.com>
2022-01-20 13:31:47 -0800

    commit tree 6183e749a575d5d469a2d5d0991f045b9d04cb3b

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let change = document.querySelector("#text")
+    change.textContent = 'This is really cool!'
+    change.append('body')
+});
+
